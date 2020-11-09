25d0e9423e559fb08f7a7964c585a8bdce361ec6
Ewelina Z <narcyza1989@o2.pl>
2020-11-09 20:13:08 +0100

    add user entity

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,12 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
+        $random_number = rand(0,100);
+
+        $numbers = [
+            'first' => 1,
+            'second' => 2,
+            'third' => 3,
+        ];
+
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'random_number' => $random_number, 'numbers' => $numbers
         ]);
     }
 }
