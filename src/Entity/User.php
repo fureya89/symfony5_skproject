5f5f0f008c6e59fe6e4397bb2b0e80fa644efd02
Ewelina Z <narcyza1989@o2.pl>
2020-11-13 21:44:38 +0100

    add registration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"username"}, message="Już istnieje użytkownik o takim loginie")
  */
 class User implements UserInterface
 {
@@ -45,6 +47,11 @@ class User implements UserInterface
      * @ORM\OneToMany(targetEntity=Photo::class, mappedBy="user")
      */
     private $photos;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -162,6 +169,18 @@ class User implements UserInterface
                 $photo->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
