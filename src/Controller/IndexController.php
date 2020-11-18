a01a1470eafd776f3382aeb2ebb23613fe17149e
Ewelina Z <narcyza1989@o2.pl>
2020-11-18 20:13:48 +0100

    add upload photo - templates, controllers and flashes

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
+use App\Form\UploadPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +14,48 @@ class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param Request $request
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('index/index.html.twig', [
 
+        $form = $this->createForm(UploadPhotoType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            if ($this->getUser()){
+                /** @var UploadedFile $pictureFileName */
+                $pictureFileName = $form->get('filename')->getData();
+
+                if($pictureFileName){
+                    try {
+                        $originalFileName = pathinfo($pictureFileName->getClientOriginalName(),PATHINFO_FILENAME);
+                        $saveFileName = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFileName);
+                        $newFileName = $saveFileName.'_'.uniqid().'.'.$pictureFileName->guessExtension();
+                        $pictureFileName->move('images/hosting/',$newFileName);
+
+                        $entityPhotos = new Photo();
+                        $entityPhotos->setFilename($newFileName);
+                        $entityPhotos->setIsPublic($form->get('is_public')->getData());
+                        $entityPhotos->setUploadedAt(new \DateTime());
+                        $entityPhotos->setUser($this->getUser());
+
+                        $entityManager = $this->getDoctrine()->getManager();
+                        $entityManager->persist($entityPhotos);
+                        $entityManager->flush();
+
+                        $this->addFlash('success', 'Udało się dodać zdjęcie');
+                    } catch(\Exception $e){
+                        $this->addFlash('error', 'Wystąpił nieoczekiwany błąd');
+                    }
+                }
+            }
+        }
+
+        return $this->render('index/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,9 +30,6 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
     {
-        if ($this->getUser()){
-            return $this->redirectToRoute('index');
-        }
 
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
