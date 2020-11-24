629c5cecc16a617c0d7bc6f7ec619f1aaed60e38
Ewelina Z <narcyza1989@o2.pl>
2020-11-24 20:54:27 +0100

    adding Protection against not logged in

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Photo;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class MyController
+ * @package App\Controller
+ * @IsGranted("ROLE_USER")
+ */
 class MyController extends AbstractController
 {
     /**
@@ -20,26 +25,23 @@ class MyController extends AbstractController
     /**
      * @Route("my/photos/set_private/{id}", name="my_photos_set_as_private")
      * @param int $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function myPhotoSetAsPrivate(int $id){
         $entityManager = $this->getDoctrine()->getManager();
         $myPhoto = $entityManager->getRepository(Photo::class)->find($id);
-        if($this->getUser()){
-            if($this->getUser() == $myPhoto->getUser()){
-                try{
-                    $myPhoto->setIsPublic(0);
-                    $entityManager->persist($myPhoto);
-                    $entityManager->flush();
-                    $this->addFlash('success', 'Zdjęcie zostało ustawione jako prywatne');
 
-                }catch(\Exception $e){
-                    $this->addFlash('error', 'Wystąpił problem przy ustawianiu jako prywatne');
-                }
-            }else{
-                $this->addFlash('error', 'Nie jesteś właścicielem tego zdjecia');
+        if($this->getUser() == $myPhoto->getUser()){
+            try{
+                $myPhoto->setIsPublic(0);
+                $entityManager->persist($myPhoto);
+                $entityManager->flush();
+                $this->addFlash('success', 'Zdjęcie zostało ustawione jako prywatne');
+            }catch(\Exception $e){
+                $this->addFlash('error', 'Wystąpił problem przy ustawianiu jako prywatne');
             }
         }else{
-            $this->addFlash('error', 'Nie jesteś zalogowany!');
+            $this->addFlash('error', 'Nie jesteś właścicielem tego zdjecia');
         }
 
         return $this->redirectToRoute('latest_photos');
@@ -48,29 +50,25 @@ class MyController extends AbstractController
     /**
      * @Route("my/photos/set_public/{id}", name="my_photos_set_as_public")
      * @param int $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function myPhotoSetAsPublic(int $id){
         $entityManager = $this->getDoctrine()->getManager();
         $myPhoto = $entityManager->getRepository(Photo::class)->find($id);
 
-        if($this->getUser()){
-            if($this->getUser() == $myPhoto->getUser()){
-                try{
-                    $myPhoto->setIsPublic(1);
-                    $entityManager->persist($myPhoto);
-                    $entityManager->flush();
-                    $this->addFlash('success', 'Zdjęcie zostało ustawione jako publiczne');
+        if($this->getUser() == $myPhoto->getUser()){
+            try{
+                $myPhoto->setIsPublic(1);
+                $entityManager->persist($myPhoto);
+                $entityManager->flush();
+                $this->addFlash('success', 'Zdjęcie zostało ustawione jako publiczne');
 
-                }catch(\Exception $e){
-                    $this->addFlash('error', 'Wystąpił problem przy ustawianiu jako publiczne');
-                }
-            }else{
-                $this->addFlash('error', 'Nie jesteś właścicielem tego zdjecia');
+            }catch(\Exception $e){
+                $this->addFlash('error', 'Wystąpił problem przy ustawianiu jako publiczne');
             }
         }else{
-            $this->addFlash('error', 'Nie jesteś zalogowany!');
+            $this->addFlash('error', 'Nie jesteś właścicielem tego zdjecia');
         }
-
 
         return $this->redirectToRoute('latest_photos');
     }
@@ -78,32 +76,31 @@ class MyController extends AbstractController
     /**
      * @Route("my/photos/remove/{id}", name="my_photos_remove")
      * @param int $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function myPhotoRemove(int $id){
         $entityManager = $this->getDoctrine()->getManager();
         $myPhotoToRemove = $entityManager->getRepository(Photo::class)->find($id);
-        if($this->getUser()){
-            if($this->getUser() == $myPhotoToRemove->getUser()){
-                try{
-                    $fileManager = new Filesystem();
-                    $fileManager->remove('images/hosting/'.$myPhotoToRemove->getFilename());
-                    if ($fileManager->exists('images/hosting'.$myPhotoToRemove->getFilename())){
-                        $this->addFlash('error', 'Nie udało się usunąć zdjęcia.');
-                    } else {
-                        $entityManager->remove($myPhotoToRemove);
-                        $entityManager->flush();
-                    }
 
-                }catch(\Exception $e) {
-                    $this->addFlash('error', 'Wystąpił problem podczas usuwania zdjęcia');
+        if($this->getUser() == $myPhotoToRemove->getUser()){
+            try{
+                $fileManager = new Filesystem();
+                $fileManager->remove('images/hosting/'.$myPhotoToRemove->getFilename());
+                if ($fileManager->exists('images/hosting'.$myPhotoToRemove->getFilename())){
+                    $this->addFlash('error', 'Nie udało się usunąć zdjęcia.');
+                } else {
+                    $entityManager->remove($myPhotoToRemove);
+                    $entityManager->flush();
                 }
 
-            }else{
-                $this->addFlash('error', 'Nie jesteś właścicielem zdjęcia - nie możesz go usunąć');
+            }catch(\Exception $e) {
+                $this->addFlash('error', 'Wystąpił problem podczas usuwania zdjęcia');
             }
+
         }else{
-            $this->addFlash('error', 'Nie jesteś zalogowany!');
+            $this->addFlash('error', 'Nie jesteś właścicielem zdjęcia - nie możesz go usunąć');
         }
+
         return $this->redirectToRoute('latest_photos');
     }
 }
